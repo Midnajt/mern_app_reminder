4e31cb7f78be6939a87b6ea44c89b057504cf850
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-12-27 15:12:24 +0100

    update

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,31 @@
+import Wrapper from '../assets/wrappers/LandingPage.js';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import Logo from '../components/Logo.jsx';
+
 const Landing = () => {
-  return <h1>Landing page</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga laborum nihil at nesciunt officia alias facilis modi ea, quos esse illum inventore provident sint vel earum repre</p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/register" className="btn login-link">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,30 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Logo } from '../components';
+import FormRow from '../components/FormRow';
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register page</h1>
-      <Link to="/login">Login page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="Johny"></FormRow>
+        <FormRow type="text" name="lastName" labelText="Last Name" defaultValue="Smith"></FormRow>
+        <FormRow type="text" name="location" defaultValue="earth"></FormRow>
+        <FormRow type="email" name="email" defaultValue="j.smith@gmail.com"></FormRow>
+        <FormRow type="password" name="password" defaultValue="start!125"></FormRow>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
