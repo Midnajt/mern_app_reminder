08092345ebf828eb6887a6a844b37bbf13c654d9
Marcin K <m.krzysztoszek@cermotor.com.pl>
2025-01-03 15:11:48 +0100

    navigation creaeted

---
+++ client/src/components/SmallSidebar.jsx
@@ -0,0 +1,7 @@
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
+
+const SmallSidebar = () => {
+  return <Wrapper>SmallSidebar</Wrapper>;
+};
+export default SmallSidebar;
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,5 +1,46 @@
+import { Outlet } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Dashboard';
+import { BigSidebar, Navbar, SmallSidebar } from '../components';
+import { createContext, useContext, useState } from 'react';
+
+const DashboardContext = createContext();
+
 const DashboardLayout = () => {
-  return <h1>DashboardLayout page</h1>;
+  // temp
+  const user = { name: 'john' };
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    console.log('toggle dark theme');
+  };
+
+  const toggleSidebar = () => {
+    console.log('toggleSidebar() z dashboardlayout.jsx');
+    setShowSidebar(!showSidebar);
+  };
+
+  const logoutUser = () => {
+    console.log('logoutUser() z dashboardlayout.jsx');
+  };
+
+  return (
+    <DashboardContext.Provider value={{ user, showSidebar, isDarkTheme, toggleDarkTheme, toggleSidebar, logoutUser }}>
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
+  );
 };
 
+export const useDashboardContext = () => useContext(DashboardContext);
 export default DashboardLayout;
