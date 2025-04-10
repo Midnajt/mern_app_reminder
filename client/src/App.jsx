d417d6b5cd736645cd25f44b5fcce6baa9b441a9
Marcin K. <m.krzysztoszek@cermotor.com.pl>
2025-04-10 08:25:43 +0200

    section job pages finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,17 @@ import {
   AllJobs,
   Profile,
   Admin,
+  EditJob,
 } from './pages';
 
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
+import { loader as allJobsLoader } from './pages/AllJobs';
 import { action as addJobAction } from './pages/AddJob';
+import { loader as editJobLoader } from './pages/EditJob';
+import { action as editJobAction } from './pages/EditJob';
+import { action as deleteJobAction } from './pages/DeleteJob';
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -63,6 +68,7 @@ const router = createBrowserRouter([
           {
             path: 'all-jobs',
             element: <AllJobs />,
+            loader: allJobsLoader,
           },
           {
             path: 'profile',
@@ -71,6 +77,16 @@ const router = createBrowserRouter([
           {
             path: 'admin',
             element: <Admin />,
+          },
+          {
+            path: 'edit-job/:id',
+            element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
+          },
+          {
+            path: 'delete-job/:id',
+            action: deleteJobAction,
           },
         ],
       },
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,6 @@ export { default as FormRowSelect } from './FormRowSelect.jsx';
 export { default as BigSidebar } from './BigSidebar.jsx';
 export { default as SmallSidebar } from './SmallSidebar.jsx';
 export { default as Navbar } from './Navbar.jsx';
+export { default as JobsContainer } from './JobsContainer.jsx';
+export { default as SearchContainer } from './SearchContainer.jsx';
+export { default as Job } from './Job.jsx';
